525422b3b5ccbbdfc3a61e7de33e01c2992ff3f1
Maxime Verhoeve <maxime.verhoeve@wegroup.be>
2022-10-16 18:17:57 +0200

    made component cleaner

--- src/components/socket_chat/ChatMessages.tsx
+++ src/components/socket_chat/ChatMessages.tsx
@@ -10,25 +10,39 @@ interface Props {
 const ChatMessages: React.FC<Props> = ({ messages, nickname }) => {
   return (
     <VStack align="stretch">
-      {messages.map(({ message, nickname: sender }, key) => {
+      {messages.map((message, i) => {
+        const { nickname: sender } = message;
         const isSenderMe = sender === nickname;
+
         return (
-          <SlideFade in={!!message} key={`message_${key}`} unmountOnExit>
-            <Text color="gray.500">
-              <Text
-                as="span"
-                mr="2"
-                color={isSenderMe ? 'gray.500' : 'gray.400'}
-                fontWeight={isSenderMe ? 'unset' : 'bold'}
-              >
-                {sender}:
-              </Text>
-              <Text as="span">{message}</Text>
-            </Text>
+          <SlideFade key={`message_${i}`} in={true} unmountOnExit>
+            <ChatMessage message={message} isSenderMe={isSenderMe} />
           </SlideFade>
         );
       })}
     </VStack>
+  );
+};
+
+interface ChatMessageProps {
+  message: Message;
+  isSenderMe: boolean;
+}
+
+const ChatMessage: React.FC<ChatMessageProps> = ({ message, isSenderMe }) => {
+  const { message: msg, nickname: sender } = message;
+  return (
+    <Text color="gray.500">
+      <Text
+        as="span"
+        mr="2"
+        color={isSenderMe ? 'gray.500' : 'gray.400'}
+        fontWeight={isSenderMe ? 'unset' : 'bold'}
+      >
+        {sender}:
+      </Text>
+      <Text as="span">{msg}</Text>
+    </Text>
   );
 };
 
