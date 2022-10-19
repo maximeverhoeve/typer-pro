72722bfc79d5294cccc8d0e263a00eb82404a40d
Maxime Verhoeve <maxime.verhoeve@wegroup.be>
2022-10-20 01:32:31 +0200

    made v0.5.0 setup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ const Header: React.FC = () => {
         - TyperPro
       </Heading>
       <Text align="right" color="gray.400" fontSize="sm">
-        v0.4.0
+        v0.5.0
       </Text>
     </HStack>
   );
