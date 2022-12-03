65636c62165cec7ed80b739304688bf60d02e424
Maxime Verhoeve <maxime.verhoeve@wegroup.be>
2022-12-03 11:44:40 +0100

    configured persist in zustand store

--- src/store/usePlayerStore.ts
+++ src/store/usePlayerStore.ts
@@ -1,13 +1,25 @@
 import create from 'zustand';
+import { persist, PersistOptions } from 'zustand/middleware';
+import { StateCreator } from 'zustand/vanilla';
 
 interface PlayerState {
   nickname: string;
   setNickname: (name: string) => void;
 }
 
-const usePlayerStore = create<PlayerState>((set) => ({
-  nickname: 'devmax',
-  setNickname: (name) => set(() => ({ nickname: name })),
-}));
+type MyPersist = (
+  config: StateCreator<PlayerState>,
+  options: PersistOptions<PlayerState>,
+) => StateCreator<PlayerState>;
+
+const usePlayerStore = create<PlayerState>(
+  (persist as MyPersist)(
+    (set) => ({
+      nickname: 'devmax',
+      setNickname: (name) => set(() => ({ nickname: name })),
+    }),
+    { name: 'player-state', getStorage: () => sessionStorage },
+  ),
+);
 
 export default usePlayerStore;
