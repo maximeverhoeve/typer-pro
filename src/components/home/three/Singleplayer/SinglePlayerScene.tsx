4223191942e301287ab44677a88d54fd99f01ac5
Maxime Verhoeve <maxime.verhoeve@wegroup.be>
2023-02-21 15:35:17 +0100

    added moving animation to player

--- src/components/home/three/Singleplayer/SinglePlayerScene.tsx
+++ src/components/home/three/Singleplayer/SinglePlayerScene.tsx
@@ -1,10 +1,13 @@
+import React, { useRef } from 'react';
 import { useBoolean } from '@chakra-ui/react';
 import { a, useSpring } from '@react-spring/three';
-import React from 'react';
+import { useFrame } from '@react-three/fiber';
 import useSinglePlayerStore from '../../../../store/useSinglePlayerStore';
 import Player from '../Player';
+import { Mesh } from 'three';
 
 const SinglePlayerScene: React.FC = () => {
+  const playerRef = useRef<Mesh>(null);
   const [isMoving, setIsMoving] = useBoolean();
   const progress = useSinglePlayerStore((state) => state.progress);
   const AnimatedPlayer = a(Player);
@@ -16,7 +19,7 @@ const SinglePlayerScene: React.FC = () => {
         tension: 100,
         friction: 20,
         precision: 0.01,
-        duration: 500,
+        duration: 400,
       },
       onStart: setIsMoving.on,
       onRest: setIsMoving.off,
@@ -24,9 +27,19 @@ const SinglePlayerScene: React.FC = () => {
     [progress],
   );
 
+  useFrame((props, delta) => {
+    if (playerRef.current) {
+      playerRef.current.rotation.y += delta * 0.5;
+    }
+  });
+
   return (
     <>
-      <AnimatedPlayer scale={scale} color={isMoving ? '#0F0' : '#F00'} />
+      <AnimatedPlayer
+        ref={playerRef}
+        scale={scale}
+        color={isMoving ? '#00CACA' : '#DC0077'}
+      />
     </>
   );
 };
