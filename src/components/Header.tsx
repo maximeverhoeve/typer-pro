2b28a34461f965e17319cedc80c50a2d7ffd7371
Maxime Verhoeve <maxime.verhoeve@wegroup.be>
2023-05-09 22:01:14 +0200

    added button for player in the top right corner

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,16 @@
-import { Box, Flex, Grid, IconButton, SlideFade, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  Grid,
+  IconButton,
+  SlideFade,
+  Text,
+} from '@chakra-ui/react';
 import React from 'react';
 import { AiFillHome } from 'react-icons/ai';
 import { motion } from 'framer-motion';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import HeaderLogo from './HeaderLogo';
 import { getDisplayName } from '../features/singleplayer/utils/playerUtils';
 import usePlayerStore from '../store/usePlayerStore';
@@ -55,18 +63,27 @@ const Header: React.FC<Props> = ({
         <HeaderLogo />
         <SlideFade in={!!nickname}>
           <Flex justify="flex-end" align="flex-start" p="4">
-            <Text
-              sx={{
-                span: {
-                  fontSize: '12px',
-                  opacity: 0.4,
-                  ml: 1,
-                },
-              }}
-              dangerouslySetInnerHTML={{
-                __html: getDisplayName({ id, name: nickname || 'Typer Pro' }),
-              }}
-            />
+            <Button
+              as={Link}
+              to="/player"
+              variant="ghost"
+              _hover={{ bg: 'gray.800' }}
+            >
+              <Text
+                transition="0.2s"
+                sx={{
+                  span: {
+                    fontSize: '12px',
+                    fontWeight: 400,
+                    opacity: 0.4,
+                    ml: 1,
+                  },
+                }}
+                dangerouslySetInnerHTML={{
+                  __html: getDisplayName({ id, name: nickname || 'Typer Pro' }),
+                }}
+              />
+            </Button>
           </Flex>
         </SlideFade>
       </Grid>
