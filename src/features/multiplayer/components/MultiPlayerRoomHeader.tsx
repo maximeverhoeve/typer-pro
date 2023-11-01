d6e3f786a64645b3e8c4cc8fe9c6b2914d2f0a3b
Maxime Verhoeve <maxime.verhoeve@wegroup.be>
2023-11-01 01:25:31 +0100

    cleaned up room component

---
+++ src/features/multiplayer/components/MultiPlayerRoomHeader.tsx
@@ -0,0 +1,27 @@
+import { Button, HStack, Text } from '@chakra-ui/react';
+import React from 'react';
+import { HiPlus } from 'react-icons/hi';
+
+interface Props {
+  onAddRoom: () => void;
+}
+
+const MultiPlayerRoomHeader: React.FC<Props> = ({ onAddRoom }) => {
+  return (
+    <HStack spacing="4" justify="space-between">
+      <Text fontSize="2xl" fontWeight="600">
+        Online rooms
+      </Text>
+      <Button
+        size="md"
+        variant="outline"
+        onClick={() => null}
+        leftIcon={<HiPlus />}
+      >
+        Add room
+      </Button>
+    </HStack>
+  );
+};
+
+export default MultiPlayerRoomHeader;
--- src/features/multiplayer/index.tsx
+++ src/features/multiplayer/index.tsx
@@ -2,38 +2,12 @@ import { motion } from 'framer-motion';
 import React from 'react';
 import MultiplayerRoomTable from './components/room-table/MultiplayerRoomTable';
 import { Room } from './types/RoomTypes';
-import { Box, Button, Center, HStack, Text, VStack } from '@chakra-ui/react';
-import { HiPlus } from 'react-icons/hi';
+import { Center, Text, VStack } from '@chakra-ui/react';
+import MultiPlayerRoomHeader from './components/MultiPlayerRoomHeader';
 
 const Multiplayer: React.FC = () => {
   // Check if socket server is connecting, show loading
   const rooms: Room[] = [
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
-    { name: 'room 1', count: 3 },
     { name: 'room 1', count: 3 },
     { name: 'room 1', count: 3 },
     { name: 'room 1', count: 3 },
@@ -61,21 +35,11 @@ const Multiplayer: React.FC = () => {
     >
       <Center>
         <VStack spacing="4" align="stretch" maxW="2xl" w="100%">
-          <HStack spacing="4" justify="space-between">
-            <Text fontSize="2xl" fontWeight="600">
-              Online rooms
-            </Text>
-            <Button
-              size="md"
-              variant="outline"
-              onClick={() => null}
-              leftIcon={<HiPlus />}
-            >
-              Add room
-            </Button>
-          </HStack>
+          <MultiPlayerRoomHeader onAddRoom={() => null} />
           <MultiplayerRoomTable rooms={rooms} onAddRoom={() => null} />
-          <Text align="center">{rooms.length} rooms online</Text>
+          {rooms.length && (
+            <Text align="center">{rooms.length} rooms online</Text>
+          )}
         </VStack>
       </Center>
     </motion.div>
